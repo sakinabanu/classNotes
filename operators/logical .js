5f8f79a68c74fa1392cea9126afdeaead16bd5a8
sakinabanu <banusakina04@gmail.com>
2023-07-29 03:01:36 +0530

    notes

---
+++ operators/logical .js
@@ -0,0 +1,17 @@
+// let a =2
+// let b = 3
+// let c = "2"
+// let d = "3"
+// let e =2
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === a);
+// console.log(a !== b);
+// console.log(a === c);
+// console.log(a === e);
+// console.log(d === e);
+// console.log(c == a);
+// console.log(a >= c);
+// console.log(a <= b);
+// console.log(a > d);
+// // console.log(a < b);
